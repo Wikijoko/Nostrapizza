c9909d4ee86e7885e94534cd1483a9cc3dc3aa86
Wikijoko <98783004+Wikijoko@users.noreply.github.com>
2022-09-12 17:19:16 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -8,5 +8,26 @@ let menu = document.getElementById("menuEntradas");
 
 	function devolver() {
 		document.getElementById("menuEntradas").style.left= "-100%";
-		document.getElementById("entradas").style.display= "flex"
+		document.getElementById("menuSaladas").style.left= "-100%";
+		document.getElementById("menuPizzas").style.left= "-100%";
+		document.getElementById("menuSobremesas").style.left= "-100%";
+		document.getElementById("menuBebidas").style.left= "-100%";
+		document.getElementById("entradas").style.display= "flex";
+	}
+
+	function menu2 () {
+		document.getElementById("menuSaladas").style.left= "0px";
+		document.getElementById("entradas").style.display= "none"
+	}
+	function menu3 () {
+		document.getElementById("menuPizzas").style.left= "0px";
+		document.getElementById("entradas").style.display="none"
+	}
+	function menu4 () {
+		document.getElementById("menuSobremesas").style.left= "0px";
+		document.getElementById("entradas").style.display="none"
+	}
+	function menu5 (){
+		document.getElementById("menuBebidas").style.left= "0px";
+		document.getElementById("entradas").style.display="none"
 	}
